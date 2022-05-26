0fbcd974614886a9e98dc5d8d7e0c23732c448ea
Bruce-N-Smith <bruce.n.smith@gov.bc.ca>
2022-05-26 15:12:10 -0700

    Update notes in Get GitHub Image
    typo, and one clarification

--- GetGitHubAvatarImage.py
+++ GetGitHubAvatarImage.py
@@ -10,12 +10,12 @@ github_user = input('Input GitHub User Name: ')
 #next up are some steps for getting web content, here is a concat of the last variable with a base url
 url = 'https://github.com/'+github_user
 
-#I think this is a variable that contains a method that sends a GET requests to GitHub via the requests library functionality
+#I think this is a variable that contains a method that sends a GET request to GitHub via the requests library functionality
 r = requests.get(url)
 
 #here's another variable I think, and what it does is use the BeautifulSoup functionality to make the request and display the HTTP content of the whole profile page, not just a code
 soup = bs(r.content, 'html.parser')
 
-#BeautifulSoup also has functionality to read through the html file for a tag, class, attribute, etc.
+#BeautifulSoup also has functionality to read through the html file for a tag, class, attribute, etc. So you can use a browser Dev tools to narrow down what image you want
 profile_image = soup.find('img', {'alt' : 'Avatar'})['src']
 print(profile_image)
