b500032d3491768364bc9218f255f90c2fcf8d59
Bruce-N-Smith <bruce.n.smith@gov.bc.ca>
2022-06-14 15:29:27 -0700

    Updated comments for MadLibs.py
    dropped a note about VSCode terminal cleanliness

--- madlibs.py
+++ madlibs.py
@@ -13,11 +13,11 @@ famous_person = input("Famous person: ")
 
 # make a variable that contains a function, string, and another variable
 # note the backslash acts as a line break in Python code, but doesn't show in the program as used by clients, it's just to help Devs
-# note when I run this in Python 3.10.4 it complains about the next line and an unterminated string literal and points to the f character
 madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \ 
 I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
 
 print(madlib)
 # end program
 # if you copy all this text and drop it in a .py file in your VSCode you can run it in a terminal there for testing and tweaking
+# don't forget to delete your old terminal data because that could generate strange errors that aren't related to this code
 # packaging this code up so that it can run on a web page or a desktop is a whole other bit of work
